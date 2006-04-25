2b99956e53f1c49226b249041262f04a736deb89
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2006-04-25 17:45:27 +0000

    * A validacao do cpf foi colocada dentro do modelo * Agora eh verificado se os registros foram salvos no banco de dados

--- test/cpf_test.rb
+++ test/cpf_test.rb
@@ -4,27 +4,21 @@ require '../lib/validates_as_cpf'
 # Modelo
 class CPFData < ActiveRecord::Base
   set_table_name "cpfs"
+  
+  validates_as_cpf :cpf
 end
 
 # Testes
 class CPFsTest < Test::Unit::TestCase
   def test_cpf_invalido
-    CPFData.validates_as_cpf :cpf
-
     cpf_invalido = CPFData.create(:id => 1, :cpf => "542.123.456-98")
-    cpf_invalido.save
-
-    assert ( cpf_invalido.errors.on(:cpf) != nil ),
-             "CPF invalido foi salvo."
+ 
+    assert ( not cpf_invalido.save ), "CPF invalido foi salvo."
   end
 
   def test_cpf_valido
-    CPFData.validates_as_cpf :cpf
-
     cpf_valido = CPFData.create(:id => 1, :cpf => "804.932.540-72")
-    cpf_valido.save
 
-    assert ( cpf_valido.errors.on(:cpf) == nil ),
-             "CPF valido nao foi salvo."
+    assert ( cpf_valido.save ), "CPF valido nao foi salvo."
   end
 end
