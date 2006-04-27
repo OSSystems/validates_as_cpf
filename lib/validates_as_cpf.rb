20da3f58421c2389be44db4943739b3c3af865b6
André Camargo <andre@boaideia.inf.br>
2006-04-27 03:15:07 +0000

    alterei schema da fixture para aceitar cpf nulo (pode vir a ser opcional)
    inclui alguns testes e troquei #create por #new
    
    refatorei alguma coisa do algoritmo de validacao
    
    substitui mensagem de erro em portugues para ingles

--- lib/validates_as_cpf.rb
+++ lib/validates_as_cpf.rb
@@ -30,35 +30,31 @@ module ValidaCPF
                99999999999
                00000000000}
 
-    valor = cpf.scan /[0-9]/
+    valor = cpf.scan(/[0-9]/).collect{|x| x.to_i}
     
-    if valor.length == 11
-      unless nulos.member?(valor.join)
-        valor = valor.collect{|x| x.to_i}
-       
-        # Calcula o primeiro digito verificador
+    if valor.length == 11 and not nulos.member?(valor.to_s)
+      # Calcula o primeiro digito verificador
+      soma = 0
+      0.upto(8) do |i|
+        soma += valor[i] * (10 - i)
+      end
+        
+      soma = soma - (11 * (soma/11))
+      dv1 = soma < 2 ? 0 : 11 - soma
+        
+      if dv1 == valor[9]
+        # Calcula o segundo digito verificador
         soma = 0
         0.upto(8) do |i|
-          soma += valor[i] * (10 - i)
+          soma += valor[i] * (11 - i)
         end
-        
-        soma = soma - (11 * (soma/11))
-        dv1 = soma < 2 ? 0 : 11 - soma
-        
-        if dv1 == valor[9]
-          # Calcula o segundo digito verificador
-          soma = 0
-          0.upto(8) do |i|
-            soma += valor[i] * (11 - i)
-          end
 
-          soma += dv1 * 2
-          valorr = (soma / 11) * 11
-          resultado = soma - valorr
-          dv2 = resultado < 2 ? 0 : 11 - resultado
+        soma += dv1 * 2
+        valorr = (soma / 11) * 11
+        resultado = soma - valorr
+        dv2 = resultado < 2 ? 0 : 11 - resultado
 
-          return true if dv2 == valor[10]
-        end
+        return true if dv2 == valor[10]
       end
     end
     return nil
@@ -69,13 +65,11 @@ module ActiveRecord
   module Validations
     module ClassMethods
       def validates_as_cpf(*attr_names)
-        configuration = { :message => "cpf inválido" }
+        configuration = { :message => "is invalid" }
         configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
 
         validates_each(attr_names, configuration) do |record, attr_name, value|
-          value_before_type_cast = record.send("#{attr_name}_before_type_cast")
-          
-          next if value.empty?
+          next if value.blank?
           
           unless ValidaCPF::valida_cpf(value)
             record.errors.add(attr_name, configuration[:message])
